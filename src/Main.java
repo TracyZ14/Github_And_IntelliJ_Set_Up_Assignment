c85bdb859af1a594a1d59a060857a6c1e26c6467
BT_4N2_07 <TracyZ14@nycstudents.net>
2023-09-19 13:07:18 -0400

    Added new line for favorite food

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Name: Tracy Zhou");
-        System.out.println("Favorite movie: Coco")
+        System.out.println("Favorite movie: Coco");
+        System.out.println("Favorite food: Ice cream");
     }
 }
